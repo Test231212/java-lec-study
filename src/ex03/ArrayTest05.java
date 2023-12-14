b482e50d90f09e6b9009281a3811473366c1d403
test231212 <j910219@gmail.com>
2023-12-14 17:51:57 +0900

    배열 ì, 반복문 연습

---
+++ src/ex03/ArrayTest05.java
@@ -0,0 +1,13 @@
+package ex03;
+
+public class ArrayTest05 {
+    public static void main(String[] args) {
+        double[] d = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
+
+        for(double e : d) {
+            System.out.print(e+" ");
+        }
+
+
+    }
+}
---
+++ src/ex03/PizzaTopping02.java
@@ -0,0 +1,13 @@
+package ex03;
+
+public class PizzaTopping02 {
+    public static void main(String[] args) {
+
+        String[] toppings = { "Pepperoni", "Mushrooms", "Onions", "Sausage", "bacon" };
+
+            for (int i = 0; i < toppings.length; i++) {
+                System.out.print(toppings[i] + " ");
+            }
+    }
+}
+
