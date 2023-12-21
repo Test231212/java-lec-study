0063b5a9bd9f7d9ea117241ff28e0588b514b3a8
test231212 <j910219@gmail.com>
2023-12-21 12:04:27 +0900

    계좌이체 테스ì트 수정

--- src/ex04/example/model/Account.java
+++ src/ex04/example/model/Account.java
@@ -1,9 +1,9 @@
 package ex04.example.model;
 
 public class Account {
-    final int id; // 계좌번호 (숫자 10자리로 만들어져있다.)
-    long balance;
-    int Userid; // 1
+    public final int id; // 계좌번호 (숫자 10자리로 만들어져있다.)
+    public long balance;
+    public int Userid; // 1
 
     public Account(int id, long balance, int userid) {
         this.id = id;
